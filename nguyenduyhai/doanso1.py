e073beb795c13632a1d9bcd5b9e08122bd1a220f
haind13 <nguyen.duyhai91@gmail.com>
2015-08-26 20:17:29 +0700

    update

--- nguyenduyhai/doanso1.py
+++ nguyenduyhai/doanso1.py
@@ -7,14 +7,10 @@ number = pow(X,2)
 print "Moi ban doan"
 B = input()
 B = int(B)
-
-while B != number :	#vong lap
-	print "Moi ban doan lai:"
-	B = input ()
-	B = int(B)
-	if B < number:
-		print "Ban phai doan mot so lon hon nua."
-	elif B > number:
+while B < number :	#vong lap
+	print "Ban phai doan mot so lon hon nua."
+	if B > number :
 		print "Ban can phai doan mot so nho hon."
-	else:
-		print "Chuc mung ban."
+	elif B == number:
+		print "Chuc mung ban da doan dung."
+	break
