a9bb53c306764d9015115d88f550845fe1efb2fa
cuongkma <gentachi@gmail.com>
2015-08-27 21:50:58 +0700

    cuongvm

---
+++ vumanhcuong/learnpythonthehardway.py
@@ -0,0 +1,23 @@
+
+#Bai 4 Variable and names
+
+print "Hi # there."
+print "Hens:", 100 - 34 +23
+print "Cong tru nhan chia:", 1 * 2 + 4 / 4
+cars = 100
+space_in_car = 4.0
+drivers = 40
+passengers = 90
+cars_not_driven = cars - drivers
+car_driven = drivers
+carpool_capacity = car_driven * space_in_car
+
+
+
+#Ngay 26/8
+
+
+a = 1
+while a < 10:
+print "Gia tri nay 10", a
+a = a + 1
