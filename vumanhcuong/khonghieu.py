86ea5fc431ed411d59b92dae23b18fe9d173df4e
CuongVM <gentachi@gmail.com>
2015-08-27 21:53:06 +0700

    Delete khonghieu.py

--- vumanhcuong/khonghieu.py
+++
@@ -1,7 +0,0 @@
-#Bai tap ve nha ngay 3 (20150827)
-
-a = raw_input ("Nhap vao 1 chuoi so:")
-#print "So nay:", a
-print "So ki tu a la:", a.count("a")
-print "So ki tu b la:", a.count("b")
-print "So ki tu c la:", a.count("c")
