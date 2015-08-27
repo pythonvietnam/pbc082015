ebeb55fade1a9914cc13290c53a8acf48b86e8de
thaitrungtan <haphap427@gmail.com>
2015-08-27 23:08:54 +0700

    t

---
+++ thaitrungtan/binhphuong.py
@@ -0,0 +1,8 @@
+a = input ("ban nhap mot so bat ki co 2 chu so:")
+b = input ("tinh nham binh phuong cua so do :")
+if b>a*a:
+	print "so ban doan cao hon gia tri cua binh phuong so do"
+elif b==a*a:
+	print "chinh xac"
+else:
+	print "so ban doan nho hon"
---
+++ thaitrungtan/tuoi.py
@@ -0,0 +1,7 @@
+year = input ("ban sinh nam bao nhieu :")
+print "tra loi %d " %(year)
+print "nam nay ban %d tuoi" %(2015 - year)
+if year<16:
+	print "khong duoc phep"
+else:
+	print "ok"
