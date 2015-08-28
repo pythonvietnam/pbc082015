4e60287dc5379561a82a2c855aaeec35a7547be0
chuhuyquang <huyquang194@gmail.com>
2015-08-28 16:08:59 +0700

    Update demsokitu.py

--- chuhuyquang/demsokitu.py
+++ chuhuyquang/demsokitu.py
@@ -5,5 +5,5 @@ b=raw_input ("Ki tu can kiem tra : ")
 for i in a:		
 	if i==b:
 		dem=dem+1
-		print "So lan xuat hien ki tu %s trong chuoi la %d "%(b,dem)
+print "So lan xuat hien ki tu %s trong chuoi la %d "%(b,dem)
 		
