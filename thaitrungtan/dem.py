20c5cbb190fbbb42287a457c82aa38ee3a93cf45
thaitrungtan <haphap427@gmail.com>
2015-08-28 15:02:07 +0700

    dem

---
+++ thaitrungtan/dem.py
@@ -0,0 +1,4 @@
+s = raw_input("nhap vao mot chuoi so:")
+print"so ki tu a la: ", s.count("a")
+print"so ki tu b la: ", s.count("b")
+print"so ki tu c la: ", s.count("c")
---
+++ thaitrungtan/t.py
@@ -0,0 +1,7 @@
+a = input("nhap vao mot so:")
+b = input("doan binh phuong cua no:")
+while b!=a*a:
+	print "so ban doan sai hay nhap lai"
+	b = input("doan  binh phuong cua no:")
+else:
+	print "chinh xac"
