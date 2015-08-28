a3e39d64c05134dfdc9ff28eeda564625bb5b722
thangvq88 <thangvq.hd@gmail.com>
2015-08-28 16:14:13 +0700

    BuoiHoc3

--- VuQuangThang/21082015_B1/bai2.py
+++
@@ -1,8 +0,0 @@
-year = input("Vui long nhap nam hien tai, dang yyyy  \n")
-age = input("Nhap nam sinh cua ban, dang yyyy \n ")
-a = year - age
-print "So tuoi cua ban la %d" %(a)
-if a < 16:
-	print "Nho tuoi qua, ve ti me di :)"
-else:
-	print "OK Ban da du tuoi hanh dong :D"
--- VuQuangThang/21082015_B1/bai3.py
+++
@@ -1,8 +0,0 @@
-###Tinh dien Tich##
-l = int(raw_input("Nhap chieu dai, dang so >0 \n"))
-while l<=0:
-	l=int(raw_input("Nhap LAI chieu dai, dang so >0 \n"))
-w = int(raw_input("Nhap chieu rong, dang so > 0, nho hon chieu dai. \n"))
-while w<=0 or w>=l:
-	w=int(raw_input("Nhap LAI chieu rong phai nho hon chieu dai va >0\n"))
-print "Dien Tich la: %d*%d = %d \n"%(l,w,l*w)
