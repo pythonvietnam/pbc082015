6a15d59ae4addce6c81b5aa491e075c4a9de49a4
cuongkma <gentachi@gmail.com>
2015-09-09 14:30:25 +0700

    update

---
+++ vumanhcuong/Day3/Baitap_ngay3.py
@@ -0,0 +1,9 @@
+#Bai tap:
+#nguoi dung nhap vao 1 chuoi ki tu
+#---Dem xem co bao ki tu a , b , c
+ #a = input ( "Nhap vao 1 chuoi so:" )
+ #	print "Chuoi so do la %s:",a
+
+age = input ("Ban bao nhieu tuoi?")
+#age = int(age)
+	print "Tuoi cua toi la %d?" %(age)
---
+++ vumanhcuong/Day4/bai4_string.py
@@ -0,0 +1,9 @@
+#Bai 4 string
+
+#Vi du ve cat chuoi tu dau phay 
+a = "Hom nay, thu 6"
+b = a.split(',')
+c = a.replace('Hom nay','Ngay Hom Nay')
+print b
+print c
+
