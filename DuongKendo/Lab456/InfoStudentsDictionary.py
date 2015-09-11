155802048eda09d0ca99103a988036afc2378700
Duonght <httd.tlu@gmail.com>
2015-09-11 19:43:36 +0700

    Lab456_SearchNameStudentDictionary

--- DuongKendo/Lab456/InfoStudentsDictionary.py
+++ DuongKendo/Lab456/InfoStudentsDictionary.py
@@ -1,12 +1,13 @@
-Student = dict()
+
 Students = list()
 
 while 1:
 	_question = raw_input("Ban muon nhap thong tin sinh vien? ")
-	if _question == 'n'
+	if _question == 'n':
 		print Students
 		break
-	elif _question =='y'
+	elif _question =='y':
+		Student = dict()
 		_hoTen = raw_input("\tHo va ten:\t")
 		_ngaySinh = raw_input("\tNgay sinh:\t")
 		_gioiTinh = raw_input("\tGioi Tinh:\t")
@@ -15,12 +16,24 @@ while 1:
 		_queQuan = raw_input("\tQue quan:\t")
 		
 		Student['hoTen'] = _hoTen
-		Student['hoTen'] = _hoTen
-		Student['hoTen'] = _hoTen
-		Student['hoTen'] = _hoTen
-		Student['hoTen'] = _hoTen
-		Student['hoTen'] = _hoTen
+		Student['ngaySinh'] = _ngaySinh
+		Student['gioiTinh'] = _gioiTinh
+		Student['diacChi'] = _diacChi
+		Student['lopHoc'] = _lopHoc
+		Student['queQuan'] = _queQuan
 		
 		Students.append(Student)
 		
+_searchNameStudent = raw_input("Nhap vao ten sinh vien can tim kiem: ")
+print _searchNameStudent
+print len(Students)
+SearchStudents = list()
+for k in Students:
+	if _searchNameStudent == k['hoTen']:
+		SearchStudents.append(k)
+		continue
+#	else:
+#		print "Sinh vien %s khong co trong danh sach"		
 
+print SearchStudents
+	 
