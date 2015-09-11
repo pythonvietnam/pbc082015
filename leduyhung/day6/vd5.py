d665d6e14257fdf79e9fe28609e335b4d34bafce
duyhung142 <duyhung142@gmail.com>
2015-09-11 17:48:07 +0700

    abc

--- leduyhung/day6/vd5.py
+++ leduyhung/day6/vd5.py
@@ -4,8 +4,6 @@
 Bai toan nhap va hien thi thong tin nguoi dung:
 -----------------------------------------------
 '''
-L=list()
-d=dict()
 print'''
 Nhap vao thong tin cua cua nguoi dung bao gom:
 1. Ho ten
@@ -15,21 +13,39 @@ Nhap vao thong tin cua cua nguoi dung bao gom:
 5. Lop hoc
 6. Que quan
 '''
+l=list()
+ds=dict()
+l1=list()
 while 1:
-	g=raw_input("Ban co muon nhap tiep du lieu nguoi dung Y/N ")
-	if g=="n" or g=="N":
+	n = raw_input("Ban co muon nhap tiep du lieu nguoi dung (Y/N): ")
+	if n == "n" or n == "N":
+#		print l
 		break
-	elif g=="y" or g=="Y":
-		a=raw_input("Ho va Ten: ")
-		b=raw_input("Ngay sinh: ")
-		c=raw_input("Gioi tinh: ")
-		d=raw_input("Dia chi: ")
-		e=raw_input("Lop hoc: ")
-		f=raw_input("Que quan: ")
-		d['HovaTen']=a	
-		d['Ngaysinh']=b
-		d['Gioitinh']=c
-		d['Diachi']=d
-		d['Lophoc']=e
-		d['Quequan'] =f
-		L.append(d)
+	elif n == "y" or n == "Y":
+		a = raw_input("Ho va Ten: ")
+		b = raw_input("Ngay sinh: ")
+		c = raw_input("Gioi tinh: ")
+		d = raw_input("Dia chi: ")
+		e = raw_input("Lop hoc: ")
+		f = raw_input("Que quan: ")
+		ds["Ho va Ten"] = a	
+		ds['Ngay sinh'] = b
+		ds['Gioi tinh'] = c
+		ds['Dia chi'] = d
+		ds['Lop hoc'] = e
+		ds['Que quan'] = f
+		l.append(ds)
+while 1:
+	m = raw_input("Ban co muon tim kiem du lieu nguoi dung (Y/N): ")
+	if m == "n" or m == "N":
+		break
+	elif m == "y" or m == "Y":
+#		print l
+		string = str(raw_input("Search keyword: "))
+#		for s in l:
+#    			if string in str(s):
+#        		print l[s]
+#			enumerate
+		for i, j in enumerate(l):
+    			if j == string:
+        			print i
