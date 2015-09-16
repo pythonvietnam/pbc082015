9bb0a14c58afa067d1303f4863ded51130242476
haind13 <you@example.com>
2015-09-16 11:50:10 +0700

    Update BTVN Module-Function

--- nguyenduyhai/bai5/Calculator/main.py
+++ nguyenduyhai/bai5/Calculator/main.py
@@ -18,20 +18,4 @@ nhan.mul()
 from Modules import chia
 chia.div()
 
-#vong lap lua chon chuong trinh
-vonglap = 1
-choice = 0
-while vonglap == 1:
-	choice = menu()
-	if choice == 1:
-		add()
-	elif choice == 2:
-		sub()
-	elif choice == 3:
-		mul()
-	elif choice == 4:
-		div()
-	elif choice == 5:
-		vonglap = 0
-
-print "Cam on ban da su dung chuong trinh."
+	
