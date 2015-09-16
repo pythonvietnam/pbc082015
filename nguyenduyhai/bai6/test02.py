15f125e31f706920e8022094c0ff1f1f63540090
haind13 <nguyen.duyhai91@gmail.com>
2015-09-16 20:05:28 +0700

    updates

---
+++ nguyenduyhai/bai6/test02.py
@@ -0,0 +1,15 @@
+#demo csv file
+
+import csv
+
+with open('test1.csv' , 'wb') as fh:
+	writer = csv.writer(fh, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+	writer.writerow(['haind', 'fpt', 'com', 'vn', 'admin'])
+	print 'Done writing!'
+
+###read the file
+print 'Begin reading ...'
+with open('test1.csv', 'rb') as fh:
+	reader = csv.reader(fh, delimiter=',', quotechar='|')
+	for row in reader:
+		print row
